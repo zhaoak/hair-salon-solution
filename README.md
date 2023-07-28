a04800f29160bdcb4995c621c3a90efd116b70f0
Allie Zhao <allie.k.zhao@gmail.com>
2023-07-28 16:50:35 -0700

    Update README, add database schema setup file
    - updated README.md with database setup instructions
    - add allie_zhao.sql database schema file

--- README.md
+++ README.md
@@ -5,8 +5,8 @@
 ## Description
 
 Simple C#/ASP.NET web application built for use with a MySQL (or otherwise 
-compatible) database. Allows user to add and track a list of stylists and 
-clients of those stylists to the database through a web frontend. Users may 
+compatible) database. Allows user to add, edit, and track a list of stylists 
+and clients of those stylists to the database through a web frontend. Users may 
 also add and remove stylists and clients from the system.
 
 ## Technologies Used
@@ -21,16 +21,29 @@ also add and remove stylists and clients from the system.
 - clone repository to location of your choice
 - ensure .NET 6 SDK is installed and correctly configured
 - ensure proper .NET dependencies are retrieved
+    - this should happen automatically with `dotnet run`
 - navigate to `HairSalon` directory
 - in your terminal, enter `dotnet run`
 - in your browser, open `http://localhost:5001`
 
 ### Database setup
 
-- todo
-
-## Known Bugs
-
+- set up a MySQL (or otherwise client protocol compatible) database instance
+- create a new database `[your_db_name]`, using a name of your choice
+    - to do this, enter `CREATE DATABASE [your_db_name];` in your DB shell
+    - you may also use a GUI database tool like MySQL Workbench
+- import the included schema file `allie_zhao.sql` to your new database
+    - in bash (not your database shell interface), the command for this is
+    `mysql -u [your_database_login] -p [your_db_name] < allie_zhao.sql`
+    - or import using a GUI database tool
+- in the `HairSalon` directory, create the file `appsettings.json`, containing the following
+    {
+      "ConnectionStrings": {
+        "DefaultConnection": "Server=[your_database_address];Port=3306;database=[your_db_name];uid=[your_db_login];pwd=[your_db_password];"
+      }
+    }
+- substitute fields in brackets with your own database information
+- also switch port if your database is not configured to use 3306
 
 ## License
 
---
+++ allie_zhao.sql
@@ -0,0 +1,78 @@
+-- MariaDB dump 10.19-11.0.2-MariaDB, for Linux (x86_64)
+--
+-- Host: localhost    Database: allie_zhao
+-- ------------------------------------------------------
+-- Server version	11.0.2-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Clients`
+--
+
+DROP TABLE IF EXISTS `Clients`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Clients` (
+  `ClientId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  `StylistId` int(11) NOT NULL,
+  PRIMARY KEY (`ClientId`),
+  UNIQUE KEY `ClientId_UNIQUE` (`ClientId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Clients`
+--
+
+LOCK TABLES `Clients` WRITE;
+/*!40000 ALTER TABLE `Clients` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Clients` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Stylists`
+--
+
+DROP TABLE IF EXISTS `Stylists`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Stylists` (
+  `StylistId` int(11) NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) DEFAULT NULL,
+  `Description` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`StylistId`),
+  UNIQUE KEY `StylistId_UNIQUE` (`StylistId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Stylists`
+--
+
+LOCK TABLES `Stylists` WRITE;
+/*!40000 ALTER TABLE `Stylists` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Stylists` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-28 16:28:05
